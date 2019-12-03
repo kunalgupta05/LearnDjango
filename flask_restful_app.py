3c32ac772878c4a18e3ac386a7213842830b9782
Kunal Gupta <kunalgupta2k12@gmail.com>
2019-12-04 00:07:50 +0530

    Created items resource, added logic for getting list of items, code refactoring and error handling.

--- flask_restful_app.py
+++ flask_restful_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -10,17 +10,25 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.99}
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "Item {} already exists".format(name)}, 400  # HTTP status code 400 for bad request
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201   # HTTP status code 201 when some object is created
 
 
-api.add_resource(Item, '/items/<string:name>')
+class Items(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
+
+app.run(port=5000, debug=True)
