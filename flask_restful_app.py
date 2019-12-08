c1ea3d243abb60ef60c2852decf7971fe95da797
Kunal Gupta <kunalgupta2k12@gmail.com>
2019-12-08 21:52:38 +0530

    Added code for delete and put endpoints. Added code for parsing the JSON payload to create/update data which is only required, preventing the side effects.

--- flask_restful_app.py
+++ flask_restful_app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -13,6 +13,12 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannot be blank")
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,10 +28,27 @@ class Item(Resource):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "Item {} already exists".format(name)}, 400  # HTTP status code 400 for bad request
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201   # HTTP status code 201 when some object is created
+
+    def delete(self, name):
+        # to make sure the below statement does not throw any error as the items variable is defined as well as used
+        # in the same sentence
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 
 class Items(Resource):
