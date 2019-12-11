e894f5e6fa24e9ec7e9e55622c162a9b34db6378
Kunal Gupta <kunalgupta2k12@gmail.com>
2019-12-12 01:15:13 +0530

    Implemented the delete method to use the sql connection and delete the item from the database.

--- item.py
+++ item.py
@@ -2,6 +2,8 @@ import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 
+items = []
+
 
 class Item(Resource):
     parser = reqparse.RequestParser()
@@ -46,10 +48,12 @@ class Item(Resource):
         return item, 201   # HTTP status code 201 when some object is created
 
     def delete(self, name):
-        # to make sure the below statement does not throw any error as the items variable is defined as well as used
-        # in the same sentence
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "DELETE FROM items where name=?"
+        cursor.execute(query, (name,))
+        connection.commit()
+        connection.close()
         return {'message': 'item deleted'}
 
     def put(self, name):
