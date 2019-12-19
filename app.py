272f28d189c4744f1a23579ffb620811cb330aa4
Kunal Gupta <kunalgupta2k12@gmail.com>
2019-12-20 00:53:32 +0530

    Creating tables for our app through SQLAlchemy rather than running the create table script every time.

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "jose"
 api = Api(app)
 
+
+# To create the data.db file using sqlalchemy
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
